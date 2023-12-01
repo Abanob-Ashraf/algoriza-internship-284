e46666e8c881d470d2b542582d5d30c97f4a543a
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-01 09:30:37 +0200

    [Implement] UnitOfWork

---
+++ VezeetaApi.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VezeetaApi.Domain;
+using VezeetaApi.Domain.Interfaces;
+using VezeetaApi.Infrastructure.Data;
+
+namespace VezeetaApi.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly VezeetaDbContext DbContext;
+        private Dictionary<Type, object> Repositories;
+
+        public UnitOfWork(VezeetaDbContext dbContext)
+        {
+            DbContext = dbContext;
+            Repositories = new Dictionary<Type, object>();
+        }
+
+        public IBaseRepository<T> GetRepository<T>() where T : class
+        {
+            // Check if the Dictionary Key contains the Model class
+            if (Repositories.Keys.Contains(typeof(T)))
+            {
+                // Return the repository for Model class
+                return Repositories[typeof(T)] as IBaseRepository<T>;
+            }
+
+            // If the dictionary doesn't contain the Model class, create a new repository for the Model class
+            IBaseRepository<T> repo = new BaseRepository<T>(DbContext);
+            Repositories.Add(typeof(T), repo);
+            return repo;
+        }
+
+        public void SaveChanges()
+        {
+            DbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            DbContext.Dispose();
+            Repositories = null;
+        }
+    }
+}
