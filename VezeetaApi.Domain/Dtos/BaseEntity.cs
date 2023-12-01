9e45efbf44355fc479f4e8a35ce264af806d098c
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-01 05:18:52 +0200

    [Create] BaseEntity

---
+++ VezeetaApi.Domain/Dtos/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VezeetaApi.Domain.Dtos
+{
+    public class BaseEntity<T>
+    {
+        public T Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
