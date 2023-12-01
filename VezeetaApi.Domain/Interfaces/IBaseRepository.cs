71dd83b36c9467c7ee612c4ca8a34c050f4f5ea5
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-01 09:00:37 +0200

    [Add] generic IBaseRepository

---
+++ VezeetaApi.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VezeetaApi.Domain.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(int id);
+
+        Task<T> FindAsync(Expression<Func<T, bool>> criteria);
+
+        Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[] includes = null);
+
+        Task<T> AddAsync(T entity);
+
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+
+        void Update(T entity);
+
+        T Delete(int id);
+
+        void DeleteRange(IEnumerable<T> entities);
+    }
+}
