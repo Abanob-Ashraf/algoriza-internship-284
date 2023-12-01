c757044bb5b4a72720ef64eb778a7984aa9acf6a
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-01 06:08:12 +0200

    [Inject] DbContext in programCs

--- VezeetaApi/Program.cs
+++ VezeetaApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using VezeetaApi.Infrastructure.Data;
 
 namespace VezeetaApi
 {
@@ -13,6 +15,9 @@ namespace VezeetaApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<VezeetaDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
             var app = builder.Build();
 
