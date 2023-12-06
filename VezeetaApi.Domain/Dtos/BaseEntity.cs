12c69a3803ba242d8fe108d1637487475aa64ae5
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-06 07:22:57 +0200

    [Implement] IActivatable Interface in BaseEntity

--- VezeetaApi.Domain/Dtos/BaseEntity.cs
+++ VezeetaApi.Domain/Dtos/BaseEntity.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using VezeetaApi.Domain.Interfaces;
 
 namespace VezeetaApi.Domain.Dtos
 {
-    public class BaseEntity<T>
+    public class BaseEntity<T> : IActivatable
     {
         public T Id { get; set; }
 
