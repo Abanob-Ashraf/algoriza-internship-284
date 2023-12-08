c24e7e73c127577caabfe657afbf034515498458
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-08 03:20:23 +0200

    [Create & implement] Auth and account DTOs

---
+++ VezeetaApi.Domain/Dtos/AuthModelDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace VezeetaApi.Domain.Dtos
+{
+    public class AuthModelDTO
+    {
+        public DateTime ExpiredOn { get; set; }
+        public string? Token { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+        public string? Message { get; set; }
+        public List<string>? Roles { get; set; }
+
+        [JsonIgnore]
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiration { get; set; }
+    }
+}
