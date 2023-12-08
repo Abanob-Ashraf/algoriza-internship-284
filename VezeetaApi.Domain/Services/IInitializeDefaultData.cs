67c81a8f5205f60fa69638a8970176505e3257cd
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-08 02:48:02 +0200

    [create] IInitializeDefaultData interface

---
+++ VezeetaApi.Domain/Services/IInitializeDefaultData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VezeetaApi.Domain.Services
+{
+    public interface IInitializeDefaultData
+    {
+        public Task InitializeData();
+    }
+}
