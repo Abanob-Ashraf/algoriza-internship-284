f6da6ebd874b1f6977d276eb005c5580eeae9ce8
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-09 04:31:01 +0200

    RefreshToken Inhert from BaseEntity

--- VezeetaApi.Domain/Models/RefreshToken.cs
+++ VezeetaApi.Domain/Models/RefreshToken.cs
@@ -4,17 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VezeetaApi.Domain.Dtos;
 
 namespace VezeetaApi.Domain.Models
 {
-    [Owned]
-    public class RefreshToken
+    public class RefreshToken : BaseEntity<int>
     {
         public string? Token { get; set; }
         public DateTime ExpiredOn { get; set; }
         public bool IsExpired => DateTime.UtcNow >= ExpiredOn;
-        public DateTime CreatedOn { get; set; }
-        public DateTime? RevokedOn { get; set; }
-        public bool IsActive => RevokedOn is null || !IsExpired;
+        public new bool IsActive => UpdatedDate is null || !IsExpired;
+
+        public string AppUserId { get; set; }
     }
 }
