3aa63a32398f7774d46fca0c89c40581356d7ad7
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-09 05:47:10 +0200

    [Rename] AuthModelDTO to AuthDto

--- VezeetaApi.Domain/Dtos/AuthDTO.cs
+++ VezeetaApi.Domain/Dtos/AuthDTO.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+﻿using System.Text.Json.Serialization;
 
 namespace VezeetaApi.Domain.Dtos
 {
-    public class AuthModelDTO
+    public class AuthDTO
     {
         public string? Message { get; set; }
 
