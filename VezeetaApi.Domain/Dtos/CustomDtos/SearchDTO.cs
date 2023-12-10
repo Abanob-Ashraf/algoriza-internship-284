ac310c4c257a3f8e6561603fd0ae37bdfae1e42f
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-10 07:44:22 +0200

    [Add] SearchDTo

---
+++ VezeetaApi.Domain/Dtos/CustomDtos/SearchDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VezeetaApi.Domain.Dtos.CustomDtos
+{
+    public class SearchDTO
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? SpecializationName { get; set; }
+    }
+}
