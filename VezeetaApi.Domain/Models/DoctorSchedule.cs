28f3640df1f1a3351dc1805c5be13076b0da815b
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-10 07:44:46 +0200

    [Change] Day enum

--- VezeetaApi.Domain/Models/DoctorSchedule.cs
+++ VezeetaApi.Domain/Models/DoctorSchedule.cs
@@ -17,12 +17,12 @@ namespace VezeetaApi.Domain.Models
 
     public enum Day
     {
-        Saturday = 0,
-        Sunday = 1,
-        Monday = 2,
-        Tuesday = 3,
-        Wednesday = 4,
-        Thursday = 5,
-        Friday = 6,
+        Sunday = 0,
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
     }
 }
