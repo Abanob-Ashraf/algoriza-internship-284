c4b9746fc4da3edb1cce885643fcc6b60831e2d4
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-14 05:25:56 +0200

    [Create] ServiceRegistration Method to manage the injection of interfaces

---
+++ VezeetaApi.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using VezeetaApi.Domain.Services;
+using VezeetaApi.Domain;
+using VezeetaApi.Infrastructure.AutoMapperConfig;
+using VezeetaApi.Infrastructure.Repositories;
+
+namespace VezeetaApi.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, AuthRepository>();
+            services.AddScoped<ISendingEmailService, SendingEmailService>();
+
+            services.AddScoped<IInitializeDefaultData, InitializeDefaultDataRepository>();
+            services.AddHostedService<InitializeDefaultDataService>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped(typeof(IBaseService<>), typeof(BaseRepository<>));
+
+            services.AddAutoMapper(Assembly.GetAssembly(typeof(MapperProfile)));
+        }
+    }
+}
