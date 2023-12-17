65b1d23b40dd8709468ec87efd7a66849b7d953d
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-17 03:10:35 +0200

    [Create] BaseConfig Class

---
+++ VezeetaApi.Infrastructure/DataConfigurations/BaseConfig.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VezeetaApi.Domain.Dtos;
+
+namespace VezeetaApi.Infrastructure.DataConfigurations
+{
+    public class BaseConfig<TEntity, T> : IEntityTypeConfiguration<TEntity> where TEntity : BaseEntity<T>
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> entity)
+        {
+            entity
+                .HasKey(e => e.Id);
+
+            entity
+                .Property(e => e.Id)
+                .ValueGeneratedOnAdd();
+
+            entity
+                .Property(e => e.CreatedDate)
+                .HasColumnType("datetime2")
+                .HasDefaultValueSql("GETDATE()");
+
+            entity
+                .Property(e => e.UpdatedDate)
+                .HasColumnType("datetime2");
+
+            entity
+                .Property(e => e.IsActive)
+                .IsRequired(true);
+        }
+    }
+}
