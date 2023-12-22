2de042fc1f0ea92856a049f139c44754d0178899
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-12-22 05:00:01 +0200

    inject FilterTimeDTO

--- VezeetaApi.Infrastructure/ServiceRegistration.cs
+++ VezeetaApi.Infrastructure/ServiceRegistration.cs
@@ -4,6 +4,7 @@ using VezeetaApi.Domain.Services;
 using VezeetaApi.Domain;
 using VezeetaApi.Infrastructure.AutoMapperConfig;
 using VezeetaApi.Infrastructure.Repositories;
+using VezeetaApi.Domain.Dtos.CustomDtos;
 
 namespace VezeetaApi.Infrastructure
 {
@@ -16,6 +17,8 @@ namespace VezeetaApi.Infrastructure
 
             services.AddScoped<IInitializeDefaultData, InitializeDefaultDataRepository>();
             services.AddHostedService<InitializeDefaultDataService>();
+
+            services.AddScoped<FilterTimeDTO>();
 
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped(typeof(IBaseService<>), typeof(BaseRepository<>));
